b774284c990cb1949615dafa0e99a0e772e7ac90
Xebisco <vtoratnog@gmail.com>
2022-02-07 19:13:08 -0300

    Alpha 1

--- src/main/java/com/xebisco/yield/Yld.java
+++ src/main/java/com/xebisco/yield/Yld.java
@@ -2,7 +2,7 @@ package com.xebisco.yield;
 
 public class Yld {
 
-    public static final String VERSION = "4 dev3";
+    public static final String VERSION = "4 alpha1";
     public static void log(Object msg) {
         System.out.println(msg);
     }
