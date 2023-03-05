5c136e6dbd44e484ba3239001e3becaf6af5d21c
xebisco <vto6j7@gmail.com>
2023-03-05 00:47:56 -0300

    Updated docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.xebisco.yield"},{"l":"com.xebisco.yield.editor"},{"l":"com.xebisco.yield.swingimpl"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.xebisco.yield"},{"l":"com.xebisco.yield.editor"},{"l":"com.xebisco.yield.editor.old"},{"l":"com.xebisco.yield.swingimpl"}];updateSearchResults();
