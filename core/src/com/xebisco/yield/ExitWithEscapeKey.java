0b80a93fb99e615ec0a01a287ef7bc9c0da40dc6
xebisco <vto6j7@gmail.com>
2023-04-26 23:35:37 -0300

    Using method instead of a lot of getters

--- core/src/com/xebisco/yield/ExitWithEscapeKey.java
+++ core/src/com/xebisco/yield/ExitWithEscapeKey.java
@@ -22,7 +22,7 @@ package com.xebisco.yield;
 public class ExitWithEscapeKey extends SystemBehavior {
     @Override
     public void onUpdate() {
-        if(getScene().getApplication().getInputManager().getPressingKeys().contains(Input.Key.VK_ESCAPE))
+        if(getScene().getApplication().isPressingKey(Input.Key.VK_ESCAPE))
             getScene().getApplication().getApplicationManager().getManagerContext().getRunning().set(false);
     }
 }
