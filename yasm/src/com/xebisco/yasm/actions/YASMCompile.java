aacd2842d1b53bbdc8b30d8ab0dccbdce84574a6
Vítor Toledo (Xebisco) <vtoratnog@gmail.com>
2023-08-29 18:51:41 -0300

    Modernized command line tools

--- yasm/src/com/xebisco/yasm/actions/YASMCompile.java
+++ yasm/src/com/xebisco/yasm/actions/YASMCompile.java
@@ -6,17 +6,59 @@ import com.xebisco.yasm.UnknownCallException;
 import com.xebisco.yasm.calls.AllCalls;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.DeflaterOutputStream;
 
 public class YASMCompile {
+
     public static void main(String[] args) throws UnknownCallException, FileNotFoundException {
+        if (args.length == 0) {
+            printTutorial();
+        }
+
+        boolean library = false;
+        String output = "a";
+
+        Pattern argP = Pattern.compile("^(\\w+)=(\\S+)$");
+
+        for (int i = 1; i < args.length; i++) {
+            String arg = args[i];
+            Matcher matcher = argP.matcher(arg);
+            switch (matcher.group(1)) {
+                case "out" -> {
+                    output = matcher.group(2);
+                }
+                case "lib" -> {
+                    library = Boolean.parseBoolean(matcher.group(2));
+                }
+                default -> {
+                    printTutorial();
+                }
+            }
+        }
+
+
         Program program = Rd.program(readFromInputStream(new BufferedInputStream(new FileInputStream(args[0]))), AllCalls.calls());
-        File newFile = new File("a.yxc");
-        try(ObjectOutputStream oo = new ObjectOutputStream(new DeflaterOutputStream(new FileOutputStream(newFile)))) {
+
+        File outputFile = new File(output + "." + (library ? "ylib" : "yx"));
+
+        OutputStream is = library ? new FileOutputStream(outputFile) : new DeflaterOutputStream(new FileOutputStream(outputFile));
+
+        try (ObjectOutputStream oo = new ObjectOutputStream(is)) {
             oo.writeObject(program);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void printTutorial() {
+        System.err.println("Incorect usage...");
+        System.err.println("Example:");
+        System.err.println("\tyasmc input.yasm");
+        System.err.println("\tyasmc input.yasm out=output");
+        System.err.println("\tyasmc input.yasm out=output lib=true");
+        System.exit(1);
     }
 
     private static String readFromInputStream(InputStream inputStream) {
--- yasm/src/com/xebisco/yasm/actions/YASMRun.java
+++ yasm/src/com/xebisco/yasm/actions/YASMRun.java
@@ -8,23 +8,10 @@ import java.util.zip.InflaterInputStream;
 
 public class YASMRun {
     public static void main(String[] args) throws IOException {
-        try(ObjectInputStream oi = new ObjectInputStream(new InflaterInputStream(new FileInputStream(args[0])))) {
+        try(ObjectInputStream oi = new ObjectInputStream(new InflaterInputStream(new FileInputStream(args[0] + ".yx")))) {
             Rd.run(((Program) oi.readObject()).global());
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static String readFromInputStream(InputStream inputStream) {
-        StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                resultStringBuilder.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return resultStringBuilder.toString();
     }
 }
