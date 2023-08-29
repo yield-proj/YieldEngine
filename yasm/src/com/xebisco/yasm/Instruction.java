f0d8d2db32d9743f9f5f66aa58e6fa304e6dee88
Vítor Toledo (Xebisco) <vtoratnog@gmail.com>
2023-08-29 18:57:12 -0300

    Converted class to record

--- yasm/src/com/xebisco/yasm/Instruction.java
+++ yasm/src/com/xebisco/yasm/Instruction.java
@@ -2,24 +2,9 @@ package com.xebisco.yasm;
 
 import java.io.Serializable;
 
-public final class Instruction implements Serializable {
-    private final Call call;
-    private final int[] args;
-
-    public Instruction(Call call, int[] args) {
-        this.call = call;
-        this.args = args;
-    }
+public record Instruction(Call call, int[] args) implements Serializable {
 
     public void run(Program program) {
         call.run(program, args);
-    }
-
-    public Call call() {
-        return call;
-    }
-
-    public int[] args() {
-        return args;
     }
 }
